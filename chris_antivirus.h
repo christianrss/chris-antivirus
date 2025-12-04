739479bc6c55c18564b00fa78672e8a94adebfff
Christian Rafael <christianparadix@gmail.com>
2025-12-04 00:13:25 -0300

    Make database

--- chris_antivirus.h
+++ chris_antivirus.h
@@ -1,4 +1,4 @@
-/* chrisping.h */
+/* chris_antivirus.h */
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <unistd.h>
@@ -12,7 +12,7 @@
 typedef unsigned char int8;
 typedef unsigned short int int16;
 typedef unsigned int int32;
-typedef unsigned long long int64;
+typedef unsigned long long int int64;
 
 #define $1 (int8 *)
 #define $2 (int16)
@@ -22,4 +22,24 @@ typedef unsigned long long int64;
 #define $c (char *)
 #define $i (int)
 
+typedef int8 Dir[64];
+typedef int8 File[32];
+
+struct s_entry {
+    Dir *dir;
+    File *file;
+};
+typedef struct s_entry Entry;
+
+struct s_database {
+    Entry *entries;
+    int16 cap;
+    int16 num;
+};
+typedef struct s_database Database;
+
+Database mkdatabase(int16);
+void addtodb(Database,Entry);
+void destroydb(Database);
+void showdb(Database);
 int main(int,char**);
